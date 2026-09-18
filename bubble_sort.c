4c1331c58ee6f29d2ba717e4cfd4600e88dd6e26
Tanyachawla24 <tanya.25gcebds253@galgotiacollege.edu>
2026-09-18 09:44:28 +0530

    Added Bubble Sort

---
+++ bubble_sort.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n) {
+    int i, j, swap;
+    // Traverse through all array elements
+    for (i = 0; i < n - 1; i++) {
+        // Last i elements are already in place
+        for (j = 0; j < n - i - 1; j++) {
+            // Swap if the element is greater than the next
+            if (arr[j] > arr[j + 1]) {
+                swap = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = swap;
+            }
+        }
+    }
+}
+
+// Function to print the array
+void printArray(int arr[], int size) {
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+// Main function
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: \n");
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
+
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    return 0;
+}
